86c718878c40b334028ef470f30171a80e5c11fe
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 22:31:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,7 +84,9 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     true
-  else !won?(board) && !full?(board)
+  elsif !won?(board) && !full?(board)
+    false
+    else won?(board)
     false
   end
 end
