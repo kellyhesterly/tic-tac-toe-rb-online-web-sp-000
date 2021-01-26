aa09bf1c4283e37c6264174a60759e6ef205f990
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 20:52:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,9 +13,9 @@ WIN_COMBINATIONS  = [
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-  puts "#{board[0]} | #{board[1]} | #{board[2]} "
-  puts "----------"
-  puts "#{board[3]} | #{board[4]} | #{board[5]} "
-  puts "----------"
-  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
