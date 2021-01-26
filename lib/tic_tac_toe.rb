bfb67b6db969db826f7d9a1bfa6eac22866832b5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 22:09:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,11 +70,9 @@ def won?(board)
   position_2 = board[combo[1]]
   position_3 = board[combo[2]]
 
-  if position_1 == "X" && position_2 == "X" && position == "X" ||
-     position_1 == "O" && position_2 == "O" && position == "O"
+  if position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+     position_1 == "O" && position_2 == "O" && position_3 == "O"
      return combo
-   else
-     false
    end
   end
 end
