07f8205994c815df5ee85b88a2865ad98835e8d3
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 21:23:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,9 +36,10 @@ end
 
 def turn_count(board)
   counter = 0
-  board.each do |element|
+    board.each do |element|
     if element == "X" || element == "O"
     counter += 1
     end
+    return counter
   end
 end
